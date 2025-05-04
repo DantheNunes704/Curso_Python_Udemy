f6d2f3698b53797b75edb5f97f344f87ae7c036d
DantheNunes704 <nunesdanthe@gmail.com>
2025-05-04 20:47:04 -0300

    Aula sobre o tipo boolean e como usa-lo

---
+++ Curso_de_python/aula5.py
@@ -0,0 +1,21 @@
+# Tipo de dado bool -> (boolean)
+# Ao questionar algo em um programa só há duas respostas possíveis: Sim (True) e Não (False) 
+# Existem vários operadores para "Questionar", entre eles há o " == " que questiona se um valor
+# é igual a outro exe:
+
+print(3 == 4)
+print(10 < 5)
+print(4 > 5)
+print(10 > 8)
+
+print(type(10 == 10))
+print(type(True))
+
+
+# O primeiro print retorna "false", pois 3 não é igual a 4
+# Já a segunda retorna "True", pois os dois valores mencionados são iguais
+#
+#
+#
+#
+#
