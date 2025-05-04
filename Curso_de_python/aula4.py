065a9dfe7b03dce65f3ace3399b0bae6ea5ef47f
DantheNunes704 <nunesdanthe@gmail.com>
2025-05-04 20:24:03 -0300

    Aula sobre tipos int e float e função type()

---
+++ Curso_de_python/aula4.py
@@ -0,0 +1,13 @@
+# Tipos int e Float
+# Tipo int -> Define uma variável que armazena um valor inteiro
+# Tipo float -> Define uma variável que armazena um valor decimal
+# Ambos int e float são "Tipos primitivos" (built-in types) em python
+# Iportante: Sempre quando for definir números decimais, é necessário utilizar o ponto como separador decimal
+
+# A função "type" pode ser usada para que o tipo de uma variável seja retornada
+
+numero = 1.1
+
+print(type(numero))
+print(type(1.2))
+print(type("numero"))
