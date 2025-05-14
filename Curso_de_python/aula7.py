3b7116dc35a5729647063237ad92a71d855e9cce
DantheNunes137 <nunesdanthe@gmail.com>
2025-05-13 22:03:16 -0300

    Aula sobre variáveis

---
+++ Curso_de_python/aula7.py
@@ -0,0 +1,17 @@
+# Variáveis são usadas para armazenar algo em um local específico da memória do computador
+# pep8: Inicie variáveis com letras minúsculas, pode usar números e underline
+# O sinal de igual (=) serve para atribuir um valor a uma variável
+
+nome_completo = "Danthe Nunes"
+
+# Também é possível atribuir uma expressão à uma variável
+soma_dois_mais_dois = 2 + 2
+
+print(nome_completo, soma_dois_mais_dois)
+
+# Sempre dê nomes que descrevem muito bem o que sua variável tem armazenado para que possíveis confusões não ocorram
+
+idade = 18
+maior_de_idade = idade >= 18
+
+print("\nMaior de idade? ", maior_de_idade, sep = "")
---
+++ Curso_de_python/aula8(exercício).py
@@ -0,0 +1,12 @@
+nome = "Pedro"
+Sobrenome = "Venancio"
+idade = 8
+ano_nascimento = 2008
+maior_de_idade = 8 >= 18
+altura_metros = 1.2
+
+print("nome: ", nome)
+print("sobrenome: ", Sobrenome)
+print("Ano nascimento: ", ano_nascimento)
+print("É maior de idade? ", maior_de_idade)
+print("altura em metros: ", altura_metros)
